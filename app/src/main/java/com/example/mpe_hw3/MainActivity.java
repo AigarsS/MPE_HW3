eceac8a3375a07db9a7ddd23c46772b59cade311
Aigars Strods <aigars.strods@tieto.com>
2021-11-18 11:46:10 +0200

    Base for 2 activities

--- app/src/main/java/com/example/mpe_hw3/MainActivity.java
+++ app/src/main/java/com/example/mpe_hw3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mpe_hw3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.activity_main_button)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent switchActivityIntent = new Intent(MainActivity.this,  SecondActivity.class);
+                        switchActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(switchActivityIntent);
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/example/mpe_hw3/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.mpe_hw3;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
